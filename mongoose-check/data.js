1c2e0ca63938897e459d2c40ad3051e1317f77b1
sundaee1147 <Ogbodudavid10@gmail.com>
2023-07-22 12:52:25 +0100

    Mongoose Checkpoint

---
+++ mongoose-check/data.js
@@ -0,0 +1,37 @@
+exports.dataArray = [
+    { 
+        name: "Ogeleka",
+        age: 22,
+        favoriteFoods: ["Fruits", "Veggies"], 
+    },
+    { 
+        name: "Festus",
+    age: 24,
+    favoriteFoods: ["Loud", "Native Rice"],
+},
+    { 
+        name: "Shina",
+    age: 27,
+    favoriteFoods: ["Chicken", "Chips"],
+},
+    { 
+        name: "Nenna",
+        age: 28,
+        favoriteFoods: ["Medemede", "Sharwama"],
+    },
+    {
+        name: "Chidera",
+        age: 27,
+        favoriteFoods: ["Snacks", "Tea"],
+     },
+    { 
+        name: "Sehinde",
+        age: 25,
+        favoriteFoods: ["Martel", "Amala Abula"],
+    },
+    {
+        name: "Sunday",
+        age: 40,
+        favoriteFoods: ["Okra", "Beans Plantain"],
+    },
+  ];
